102e6b7085fe9d53e099f7fe8c20914c039c1e09
Praveen Chaudhary <chaudhary.praveen808@gmail.com>
2020-04-11 02:48:04 +0530

    css changed

--- MyProject/contacts/forms.py
+++ MyProject/contacts/forms.py
@@ -1,13 +1,27 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea,TextInput
 
 from . models import contact,internship
 class contactsForm(ModelForm):
     class Meta:
         model = contact
         fields = ['Name','phone',  'email','message']
+        widgets = {
+            'Name': TextInput(attrs={'placeholder': 'Name'}),
+            'phone': TextInput(attrs={'placeholder': 'Phone'}),
+            'email': TextInput(attrs={'placeholder': 'Email'}),
+            'message': Textarea(attrs={'placeholder': 'Message'}),
+
+        }
 
 class internshipForm(ModelForm):
     class Meta:
         model = internship
         fields = ['Name','phone',  'email','message','CV']
+        widgets = {
+            'Name': TextInput(attrs={'placeholder': 'Name'}),
+            'phone': TextInput(attrs={'placeholder': 'Phone'}),
+            'email': TextInput(attrs={'placeholder': 'Email'}),
+            'message': Textarea(attrs={'placeholder': 'Message'}),
+
+        }
